2c22e16ff93245a8eff5786ca8fd6bc9669d2ebf
DIEGO ALEJANDRO HERNANDEZ MARTINEZ <usercall.hm@gmail.com>
2021-10-07 23:45:47 -0500

    proyect base

---
+++ src/test/java/runners/RunnerTags.java
@@ -0,0 +1,21 @@
+package runners;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        features= "src/test/resources/features/academyChoucair.feature",
+        tags= "@Regresion",
+        glue= "co.com.choucair.certification.proyectbase.stepdefinitions",
+        snippets = SnippetType.CAMELCASE
+)
+
+public class RunnerTags {
+
+}
+
